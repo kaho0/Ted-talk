d0f0332a83c94f3dc279c4d2d98aebcfbb2d15f8
chistym17 <mchisty31@gmail.com>
2023-11-25 14:24:11 +0600

    Navbar added

--- client/src/components/Shared/Navbar/Navbar.jsx
+++ client/src/components/Shared/Navbar/Navbar.jsx
@@ -1,31 +1,34 @@
-import { Link } from 'react-router-dom'
-import Container from '../Container'
-import logoImg from '../../../assets/images/logo.png'
-import MenuDropdown from './MenuDropdown'
+import { Link } from 'react-router-dom';
+import Container from '../Container';
+import MenuDropdown from './MenuDropdown';
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full bg-white z-10 shadow-sm'>
-      <div className='py-4 border-b-[1px]'>
+    <div className='fixed w-full bg-gray text-black z-10 bg-opacity-20  shadow-sm'>
+      <div className=' border-b-[1px]'>
         <Container>
-          <div className='flex flex-row  items-center justify-between gap-3 md:gap-0'>
-            {/* Logo */}
-            <Link to='/'>
-              <img
-                className='hidden md:block'
-                src={logoImg}
-                alt='logo'
-                width='100'
-                height='100'
-              />
-            </Link>
+          <div className='flex flex-row items-center  justify-between gap-3 md:gap-0'>
+
+            {/* Restaurant Name on the Left */}
+            <div className='text-lg font-bold ml-4'>
+              TagTalk
+            </div>
+
+            {/* Left-side Links */}
+            <div className='flex space-x-4 '>
+              <Link to='/'>Home</Link>
+              <Link to='/contact-us'>Membership</Link>
+              <Link to='/dashboard'>Dashboard</Link>
+              <Link to='/menu'>Join Us</Link>
+            </div>
+
             {/* Dropdown Menu */}
             <MenuDropdown />
           </div>
         </Container>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to StayVista</h1>
     </div>
   )
 }
