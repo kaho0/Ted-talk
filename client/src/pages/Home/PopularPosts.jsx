852c5603c6f441fd7ba005ccf86873d61e4054af
chistym17 <mchisty31@gmail.com>
2023-11-30 18:34:09 +0600

    Popular posts section added

---
+++ client/src/pages/Home/PopularPosts.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import AxiosPublic from "../../Axios/AxiosBase";
+import BlogCard from "../Blogs/Blogcard";
+
+const PopularPosts = () => {
+    const [posts, setposts] = useState([])
+
+    useEffect(() => {
+        AxiosPublic.get('popularposts')
+            .then(res => {
+                setposts(res.data)
+            })
+
+
+    }, [])
+    return (
+        <div>
+   <div className="mt-10">
+
+            <div className="text-4xl font-bold text-gray-600 mb-6 text-center">
+                Popular Blog Entries
+            </div>
+
+            <div className="flex justify-between ">
+                {posts.map(post => <BlogCard  key={post._id} post={post}></BlogCard>)}
+            </div>
+            <div className="text-xl font-semibold text-gray-800 mt-2 text-center hover:text-blue-500">
+                View All Blogs
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default PopularPosts;
